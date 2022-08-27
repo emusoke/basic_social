5e12e36da22c0d21ec0871683500f68f9c307238
unknown <emusoke@gmail.com>
2022-08-27 14:38:12 +0200

    Adding security

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from fastapi import FastAPI, Depends, HTTPException
+from gettext import dpgettext
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 import psycopg2
@@ -10,6 +12,24 @@ from .database import sessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def fake_hash_password(password: str):
+    return "Hashing at the front" + password + "Hashing at the back"
+
+def fake_decode_token(token):
+    user = crud.get_user_by_username(db=db,username=username)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED, 
+            detail="Incorrect Username or Password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return user
 
 def get_db():
     db = sessionLocal()
@@ -39,3 +59,17 @@ async def read_all_posts(skip: int = 0, limit: int = 100, db: Session = Depends(
 @app.post("/posts/{user_id}/post")
 async def make_post(user_id: int, post: schemas.CreatePost,db: Session = Depends(get_db)):
     return crud.create_post(db=db,post=post,user_id=user_id)
+
+@app.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user_dict = crud.get_user_by_username(db=db, username=form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, 
+        detail="Incorrect Username or Password")
+    user = user_dict.username
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user_dict.password_hash:
+        raise HTTPException(status_code=400,
+        detail="Incorrect Username or Password")
+
+    return {"access_token": user , "token_type":"bearer"}
